f52f553d8944b78c118a8287f50dda465c065964
tobias <tobiashubelpa@gmail.com>
2023-10-31 21:40:14 -0300

    modelado de los alumnos, instructores y cursos

---
+++ cursos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Curso, Alumno, Instructor
+
+admin.site.register(Curso)
+admin.site.register(Alumno)
+admin.site.register(Instructor)
+
+
+# Register your models here.
---
+++ cursos/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class Curso(models.Model):
+    name = models.CharField(max_length=100)
+    horario = models.CharField(max_length=100)
+    dias = models.CharField(max_length=100)
+    fechaInicio = models.DateField()
+    fechaFinalizacion = models.DateField()
+    instructor = models.ForeignKey(
+        "Instructor", on_delete=models.CASCADE, blank=True, null=True
+    )
+    alumnos = models.ManyToManyField("Alumno", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Alumno(models.Model):
+    name = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    fechaNacimiento = models.DateField()
+    sexo = models.CharField(max_length=1)
+    documento = models.IntegerField()
+    domicilio = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    # cursosInscritos = models.ManyToManyField("Curso")
+
+    def __str__(self):
+        return f"{self.name} {self.apellido}"
+
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField()
+    sexo = models.CharField(max_length=1)
+    documento = models.IntegerField()
+    domicilio = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    # cursos = models.ManyToManyField("Curso")
+
+    def __str__(self):
+        return f"{self.name} {self.apellido}"
+
+
+# Create your models here.
